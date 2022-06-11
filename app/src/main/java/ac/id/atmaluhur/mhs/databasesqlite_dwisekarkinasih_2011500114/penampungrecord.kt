58c81921735f51d98ba1018c6c09146f42f7860a
USER <user@192.168.1.7>
2022-06-11 21:51:51 +0700

    Initial commit

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/databasesqlite_dwisekarkinasih_2011500114/penampungrecord.kt
@@ -0,0 +1,9 @@
+package ac.id.atmaluhur.mhs.databasesqlite_dwisekarkinasih_2011500114
+
+class MataKuliah(
+    var kdMatkul : String,
+    var nmMatkul : String,
+    var sks : Int,
+    var sifat : String
+)
+
